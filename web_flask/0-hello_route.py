0a74962869a0ca45858781175745c37741cce9de
isaac2036 <isaacpablo0596@hotmail.com>
2024-01-22 17:02:51 +0000

    Feat: Create 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Starts the flask web application.
-"""
+"""0.Write a script that starts a Flask web application"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
-#Define the route for the root URL'/'
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB! """
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    # Start the Flask development server
-    # Listen on all available network interfaces (0.0.0.0) and port 5000
     app.run(host='0.0.0.0', port=5000)
