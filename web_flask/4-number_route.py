27dd341b4b3e9cb4fb50b4a5b36947fa3f5fc3db
Jhony <Jhonny14_1@hotmail.com>
2024-01-25 08:15:33 -0500

    chore: se mdofica salida del templates

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -37,7 +37,7 @@ def Python_is_cool(text='is cool'):
 def is_it_a_number(n):
     """Python si es genial!!!"""
     if isinstance(n, int):
-        return f"{n} is number"
+        return f"{n} a is number"
 
 
 if __name__ == '__main__':
