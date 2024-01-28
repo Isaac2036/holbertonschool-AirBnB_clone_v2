547540c62715f7941859dfd47ccc40856cca51c3
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 15:53:56 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""create a unique FileStorage instance for your application"""
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+"""This module instantiates an object of class FileStorage"""
+from os import getenv
 
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+"""Check the value of the environment variable HBNB_TYPE_STORAGE"""
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
+if storage_type == "db":
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
     storage.reload()
