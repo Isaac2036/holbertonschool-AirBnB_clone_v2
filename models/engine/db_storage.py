6b64ce93fe5e40ca8ab49581d5595b6e8f168984
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 16:51:23 -0500

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,28 +1,21 @@
 #!/usr/bin/python3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
-from os import getenv
 from models.base_model import Base
+from os import getenv
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
 
 
-class DBStorage:
-    """
-    DBStorage class provides a simple interface for interacting with
-    a MySQL database
-    using SQLAlchemy. It includes methods for querying, adding,
-    saving, and deleting
-    objects in the database.
-    """
+class DBStorage():
     __engine = None
     __session = None
 
-        def __init__(self):
+    def __init__(self):
         user = getenv('HBNB_MYSQL_USER')
         pwd = getenv('HBNB_MYSQL_PWD')
         host = getenv('HBNB_MYSQL_HOST')
@@ -35,38 +28,39 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """Query all objects from the db session based on class name"""
-        classes = [User, State, City, Amenity, Place, Review]
-        result_dict = {}
+        list_classes = [User, State, City, Place, Amenity, Review]
+        classes = [cls] if cls else list_classes
 
-        if cls:
-            classes = [cls]
+        result = {}
 
-        for class_obj in classes:
-            objects = self.__session.query(class_obj).all()
-            for obj in objects:
-                key = f"{obj.__class__.__name__}.{obj.id}"
-                result_dict[key] = obj
-        return result_dict
+        for c in classes:
+            objs = self.__session.query(c).all()
+            for obj in objs:
+                key = f'{obj.__class__.__name__}.{obj.id}'
+                result[key] = obj
+
+        return result
 
     def new(self, obj):
-        """Add the object to the current database session."""
+        """Add a obj"""
         self.__session.add(obj)
 
     def save(self):
-        """Commit all changes to the current database session."""
+        """Commit session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Delete the object from the current database session."""
-        if obj is not None:
+        """Delete obj from the current database session"""
+        if obj:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables in the database and initialize a session."""
+        """Create all tables and create the session with current database"""
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
 
     def close(self):
-        self.__session.close()
+        """Remove all session"""
+        self.__session.remove()
