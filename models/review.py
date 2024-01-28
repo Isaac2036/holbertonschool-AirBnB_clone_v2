dc031605f6fedfeadf8d2cf7328ff20c4cdfa3c6
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 17:36:56 -0500

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
+from models.city import City
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    class State(BaseModel, Base):
+        """ State class for ORM"""
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+else:
+    class State(BaseModel):
+        """ State class """
+        name = ''
+
+        @property
+        def cities(self):
+            from models import storage
+            list_cities = []
+
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+
+            return list_cities
