8aba9c29049d0cf774bd0b1c16559fd1443a3a98
Isaac2036 <isaacpablo0596@hotmail.com>
2024-01-28 20:24:03 -0500

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,26 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>HBNB</title>
-  </head>
-  <body>
-    <h1>States</h1>
-    <ul>
-    {% for state in states|sort(attribute='name') %}
-      <li>{{ state.id|e }}: <b>{{ state.name|e }}</b>
-        <ul>
-        {% for city in state.cities|sort(attribute='name') %}
-            <li>{{ city.id|e }}: <b>{{ city.name|e }}</b></li>
-        {% endfor %}
-        </ul>
-      </li>
-    {% endfor %}
-    </ul>
-  </body>
-</html>
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities():
+    """Returns a rendered html template
+    at the /cities_by_states route,
+    listing the cities by states"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
